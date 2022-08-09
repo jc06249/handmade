8f5d8c018bad0352c863c547b30d02b054179aef
Jade <chittzy01@gmail.com>
2022-08-09 22:14:54 +1000

    Ep3

--- code/build.bat
+++ code/build.bat
@@ -5,5 +5,5 @@ set path=p:\handmade\misc;%path%
 
 mkdir ..\..\build
 pushd ..\..\build
-cl -Zi ..\handmade\code\win32_handmade.cpp user32.lib Gdi32.lib
+cl -Zi ..\handmade\code\win32_handmade.cpp user32.lib gdi32.lib
 popd
--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,48 +1,78 @@
 #include <windows.h>
 
-LRESULT CALLBACK
-MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam){
+#define internal static
+#define local_persist static
+#define global_variable static
+
+global_variable bool Running;
+
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+internal void Win32ResizeDIBSection(int Width, int Height){
+    if(BitmapHandle){
+        DeleteObject(BitmapHandle);
+    }
+    if(!BitmapDeviceContext){
+    BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    BitmapHandle = CreateDIBSection(BitmapDeviceContext, &BitmapInfo, DIB_RGB_COLORS, &BitmapMemory, 0, 0);
+}
+
+internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height){
+    StretchDIBits(DeviceContext, X, Y, Width, Height, X, Y, Width, Height, BitmapMemory, &BitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+}
+
+LRESULT CALLBACK Win32MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam){
     LRESULT Result = 0;
 
     switch(Message){
         case WM_SIZE:{
-            OutputDebugStringA("WM_SIZE\n");
-        }break;
-
-        case WM_DESTROY:{
-            OutputDebugStringA("WM_DESTROY\n");
-        }break;
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+            int Width = ClientRect.right - ClientRect.left;
+            int Height = ClientRect.bottom - ClientRect.top;
+            Win32ResizeDIBSection(Width, Height);
+        } break;
 
         case WM_CLOSE:{
-            OutputDebugStringA("WM_CLOSE\n");
-        }break;
+            Running = false;
+        } break;
 
         case WM_ACTIVATEAPP:{
             OutputDebugStringA("WM_ACTIVATEAPP\n");
-        }break;
+        } break;
+
+        case WM_DESTROY: {
+            Running = false;
+        } break;
 
         case WM_PAINT:{
             PAINTSTRUCT Paint;
             HDC DeviceContext = BeginPaint(Window, &Paint);
+
             int X = Paint.rcPaint.left;
             int Y = Paint.rcPaint.top;
             int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
             int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-            static DWORD Operation = WHITENESS;
-            PatBlt(DeviceContext, X,Y, Width, Height, Operation);
-            if(Operation == WHITENESS){
-                Operation = BLACKNESS;
-            }
-            else{
-                Operation = WHITENESS;
-            }
+            Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
             EndPaint(Window, &Paint);
         }
 
         default:{
             OutputDebugStringA("default\n");
             Result = DefWindowProcA(Window, Message, WParam, LParam);
-        }break;
+        } break;
     }
     return(Result);
 }
@@ -50,7 +80,7 @@ MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam){
 int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLine, int ShowCode){
     WNDCLASS WindowClass = {};
     WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
-    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.lpfnWndProc = Win32MainWindowCallback;
     WindowClass.hInstance = Instance;
     // HICON hIcon;
     WindowClass.lpszClassName = "Handmade Hero Window Class";
@@ -59,7 +89,8 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLi
         HWND WindowHandle =
             CreateWindowExA(0, WindowClass.lpszClassName, "Handmake Hero", WS_OVERLAPPEDWINDOW|WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, Instance, 0); 
         if(WindowHandle){
-            for(;;){
+            Running = true;
+            while(Running){
                 MSG Message;
                 BOOL MEssageResult = GetMessageA(&Message, 0, 0,0);
                 if(MEssageResult > 0){
