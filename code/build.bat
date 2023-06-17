587adaf161bee12a5a1cab4dc227655e1b09018a
Jade <chittzy01@gmail.com>
2023-06-17 11:29:05 +1000

    Update build.bat

--- code/build.bat
+++ code/build.bat
@@ -4,7 +4,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary
 set path=P:\handmade\misc;%path%
 
 REM W4 switch
-set CommonCompilerFlags=-MTd -nologo -Gm- -GR- -EHsc -EHa- -Od -Oi -WX -W3 -wd4201 -wd4100 -wd4189 -wd4505 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7
+set CommonCompilerFlags=-MTd -nologo -fp:fast -Gm- -GR- -EHsc -EHa- -Od -Oi -WX -W3 -wd4201 -wd4100 -wd4189 -wd4505 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7
 set CommonLinkerFlags= -incremental:no -opt:ref -subsystem:windows user32.lib gdi32.lib winmm.lib
 
 IF NOT EXIST ..\build mkdir ..\build
@@ -13,7 +13,7 @@ REM 32-bit
 REM cl %CommonCompilerFlags%  ..\handmade\code\win32_handmade.cpp /link -subsystem:windows,5.1 %CommonLinkerFlags%
 REM 64-bit
 del *.pdb > NUL 2> NUL
-REM Optimixation switches /O2 /Oi /fp:fast
+REM Optimixation switches /O2
 cl %CommonCompilerFlags% ..\handmade\code\handmade.cpp -Fmwin32_handmade.map -LD /link -incremental:no /PDB:handmade_%random%.pdb -EXPORT:GameGetSoundSamples -EXPORT:GameUpdateAndRender
 cl %CommonCompilerFlags% ..\handmade\code\win32_handmade.cpp -Fmwin32_handmade.map /link %CommonLinkerFlags%
 popd
