858994e4fe438e20c8f826fd177f015b3fd4ce5d
Jade <chittzy01@gmail.com>
2023-10-02 22:29:01 +1100

    Ep 44.

--- code/handmade.cpp
+++ code/handmade.cpp
@@ -512,9 +512,47 @@ extern "C" GAME_UPDATE_AND_RENDER(GameUpdateAndRender)
             PlayerRight.Offset.X += 0.5f * PlayerWidth;
             PlayerRight = RecanonicalizePosition(TileMap, PlayerRight);
 
-            if(IsTileMapPointEmpty(TileMap, NewPlayerP) &&
-               IsTileMapPointEmpty(TileMap, PlayerLeft) &&
-               IsTileMapPointEmpty(TileMap, PlayerRight))
+            bool32 Collided = false;
+            tile_map_position ColP = {};
+            if(!IsTileMapPointEmpty(TileMap, NewPlayerP))
+            {
+                ColP = NewPlayerP;
+                Collided = true;
+            }
+            if(!IsTileMapPointEmpty(TileMap, PlayerLeft))
+            {
+                ColP = PlayerLeft;
+                Collided = true;
+            }
+            if(!IsTileMapPointEmpty(TileMap, PlayerRight))
+            {
+                ColP = PlayerRight;
+                Collided = true;
+            }
+
+            if(Collided)
+            {
+                v2 r = {0, 0};
+                if(ColP.AbsTileX < GameState->PlayerP.AbsTileX)
+                {
+                    r = v2{1 , 0};
+                }
+                if(ColP.AbsTileX > GameState->PlayerP.AbsTileX)
+                {
+                    r = v2{-1 , 0};
+                }
+                if(ColP.AbsTileY < GameState->PlayerP.AbsTileY)
+                {
+                    r = v2{0 , 1};
+                }
+                if(ColP.AbsTileY > GameState->PlayerP.AbsTileY)
+                {
+                    r = v2{0 , -1};
+                }
+
+                GameState->dPlayerP = GameState->dPlayerP - 1 * Inner(GameState->dPlayerP, r) * r;
+            }
+            else
             {
                 if(!AreOnSameTile(&GameState->PlayerP, &NewPlayerP))
                 {
@@ -530,6 +568,7 @@ extern "C" GAME_UPDATE_AND_RENDER(GameUpdateAndRender)
                 }
                 GameState->PlayerP = NewPlayerP;
             }
+
             GameState->CameraP.AbsTileZ = GameState->PlayerP.AbsTileZ;
 
             tile_map_difference Diff = Subtract(TileMap, &GameState->PlayerP, &GameState->CameraP);
