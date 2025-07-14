33ef6226f6ca2f4cc69422d96a73db38a55934d4
Jade <chittzy01@gmail.com>
2025-07-14 21:28:47 +1000

    Ep 164.

--- code/test_asset_builder.cpp
+++ code/test_asset_builder.cpp
@@ -1,8 +1,14 @@
 
 #include "test_asset_builder.h"
 
+#define USE_FONTS_FROM_WINDOWS 1
+
+#if USE_FONTS_FROM_WINDOWS
+#include <windows.h>
+#else
 #define STB_TRUETYPE_IMPLEMENTATION 1
 #include "stb_truetype.h"
+#endif
 
 #pragma pack(push, 1)
 struct bitmap_header
@@ -188,9 +194,107 @@ internal loaded_bitmap LoadBMP(char *FileName)
     return(Result);
 }
 
-internal loaded_bitmap LoadGlyphBitmap(char *FileName, u32 Codepoint)
+internal loaded_bitmap LoadGlyphBitmap(char *FileName, char *FontName, u32 Codepoint)
 {
     loaded_bitmap Result = {};
+
+#if USE_FONTS_FROM_WINDOWS
+    static HDC DeviceContext = 0;
+    if(!DeviceContext)
+    {
+        AddFontResourceExA(FileName, FR_PRIVATE, 0);
+        int Height = 128;
+        HFONT Font = CreateFontA(Height, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, ANTIALIASED_QUALITY, DEFAULT_PITCH|FF_DONTCARE, FontName);
+
+        HDC DeviceContext = CreateCompatibleDC(0);
+        HBITMAP Bitmap = CreateCompatibleBitmap(DeviceContext, 1024, 1024);
+        SelectObject(DeviceContext, Bitmap);
+        SelectObject(DeviceContext, Font);
+        SetBkColor(DeviceContext, RGB(0, 0, 0));
+
+        TEXTMETRIC TextMetric;
+        GetTextMetrics(DeviceContext, &TextMetric);
+    }
+
+    wchar_t CheesePoint = (wchar_t)Codepoint;
+
+    SIZE Size;
+    GetTextExtentPoint32W(DeviceContext, &CheesePoint, 1, &Size);
+
+    int Width = Size.cx;
+    int Height = Size.cy;
+
+    // PatBlt(DeviceContext, 0, 0, Width, Height, BLACKNESS);
+    // SetBkMode(DeviceContext, TRANSPARENT);
+    SetTextColor(DeviceContext, RGB(255, 255, 255));
+    TextOutW(DeviceContext, 0, 0, &CheesePoint, 1);
+
+    s32 MinX = 10000;
+    s32 MinY = 10000;
+    s32 MaxX = -10000;
+    s32 MaxY = -10000;
+    for(s32 Y = 0; Y < Height; ++Y)
+    {
+        for(s32 X = 0; X < Width; ++X)
+        {
+            COLORREF Pixel = GetPixel(DeviceContext, X, Y);
+            if(Pixel != 0)
+            {
+                if(MinX > X)
+                {
+                    MinX = X;
+                }
+                if(MinY > Y)
+                {
+                    MinY = Y;
+                }
+                if(MaxX < X)
+                {
+                    MaxX = X;
+                }
+                if(MaxY < Y)
+                {
+                    MaxY = Y;
+                }
+            }
+        }
+    }
+
+    if(MinX <= MaxX)
+    {
+        --MinX;
+        --MinY;
+        ++MaxX;
+        ++MaxX;
+
+        Width = (MaxX - MinX) + 1;
+        Height = (MaxY - MinY) + 1;
+
+        Result.Width = Width;
+        Result.Height = Height;
+        Result.Pitch = Result.Width * BITMAP_BYTES_PER_PIXEL;
+        Result.Memory = malloc(Height * Result.Pitch);
+        Result.Free = Result.Memory;
+    
+        u8 *DestRow = (u8 *)Result.Memory + (Height - 1) * Result.Pitch;
+        for(s32 Y = MinY; Y <= MaxY; ++Y)
+        {
+            u32 *Dest = (u32 *)DestRow;
+            for(s32 X = MinX; X <= MaxX; ++X)
+            {
+                COLORREF Pixel = GetPixel(DeviceContext, X, Y);
+                u8 Gray = (u8)(Pixel & 0xFF);
+                u8 Alpha = 0xFF;
+                *Dest++ = ((Alpha << 24) |
+                            (Gray << 16) |
+                            (Gray <<  8) |
+                            (Gray <<  0));
+            }
+            DestRow -= Result.Pitch;
+        }
+    }
+
+#else
 
     entire_file TTFFile = ReadEntireFile(FileName);
     if(TTFFile.ContentsSize != 0)
@@ -226,6 +330,7 @@ internal loaded_bitmap LoadGlyphBitmap(char *FileName, u32 Codepoint)
         stbtt_FreeBitmap(MonoBitmap, 0);
         free(TTFFile.Contents);
     }
+#endif
 
     return(Result);
 }
@@ -430,7 +535,7 @@ internal bitmap_id AddBitmapAsset(game_assets *Assets, char *FileName, r32 Align
     return(Result);
 }
 
-internal bitmap_id AddCharacterAsset(game_assets *Assets, char *FontFile, u32 Codepoint, r32 AlignPercentageX = 0.5f ,r32 AlignPercentageY = 0.5f)
+internal bitmap_id AddCharacterAsset(game_assets *Assets, char *FontFile, char *FontName, u32 Codepoint, r32 AlignPercentageX = 0.5f ,r32 AlignPercentageY = 0.5f)
 {
     Assert(Assets->DEBUGAssetType);
     Assert(Assets->DEBUGAssetType->OnePastLastAssetIndex < ArrayCount(Assets->Assets));
@@ -446,6 +551,7 @@ internal bitmap_id AddCharacterAsset(game_assets *Assets, char *FontFile, u32 Co
     Source->Type = AssetType_Font;
     Source->FileName = FontFile;
     Source->Codepoint = Codepoint;
+    Source->FontName = FontName;
 
     Assets->AssetIndex = Result.Value;
 
@@ -523,7 +629,7 @@ internal void WriteHHA(game_assets *Assets, char *FileName)
             asset_source *Source = Assets->AssetSources + AssetIndex;
             hha_asset *Dest = Assets->Assets + AssetIndex;
 
-            Dest->DataOffset =ftell(Out);
+            Dest->DataOffset = ftell(Out);
 
             if(Source->Type == AssetType_Sound)
             {
@@ -544,7 +650,7 @@ internal void WriteHHA(game_assets *Assets, char *FileName)
                 loaded_bitmap Bitmap;
                 if(Source->Type == AssetType_Font)
                 {
-                    Bitmap = LoadGlyphBitmap(Source->FileName, Source->Codepoint);
+                    Bitmap = LoadGlyphBitmap(Source->FileName, Source->FontName, Source->Codepoint);
                 }
                 else
                 {
@@ -671,7 +777,7 @@ internal void WriteNonHero()
     BeginAssetType(Assets, Asset_Font);
     for(u32 Character = 'A'; Character <= 'Z'; ++Character)
     {
-        AddCharacterAsset(Assets, "c:/Windows/Fonts/arial.ttf", Character);
+        AddCharacterAsset(Assets, "c:/Windows/Fonts/arial.ttf", "Arial", Character);
         AddTag(Assets, Tag_UnicodeCodePoint, (r32)Character);
     }
     EndAssetType(Assets);
@@ -733,7 +839,7 @@ internal void WriteSounds()
 
 int main(int ArgCount, char **Args)
 {
-    WriteHero();
     WriteNonHero();
+    WriteHero();
     WriteSounds();
 }
